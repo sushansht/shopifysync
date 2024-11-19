4321a31ac912b0f25a808f9ef26eb886de28bfef
Sushan Shrestha <sushan.shrestha@dpl.company>
2024-11-19 13:43:31 +0545

    graph ql issue fix

--- src/Services/ProductBulkQueryService.php
+++ src/Services/ProductBulkQueryService.php
@@ -122,7 +122,7 @@ class ProductBulkQueryService
                                         }
                                     }
                                 }
-                                '{$metafieldsQuery}'
+                                {$metafieldsQuery}
                             }
                         }
                     }
